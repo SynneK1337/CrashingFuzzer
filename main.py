fe888d6acb030ecb9f6fb4726e742daa5812f824
SynneK1337 <276786@pw.edu.pl>
2018-02-19 15:35:00 +0100

    Log into FB

--- main.py
+++ main.py
@@ -7,4 +7,6 @@ config.read('config.cfg')
 login = config['LOGIN']['email']
 psw = config['LOGIN']['pass']
 victim = config['OPTIONS']['victim_id']
-
+print(psw)
+# Loging into FB
+client = fbchat.Client(login, psw)
